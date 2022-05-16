a122ed20eb4e1dc9c5a458c2cbb29c35fe31323a
AnjaP <treyjapan@pm.me>
2022-05-16 10:49:39 +0200

    adds assets folder

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import 
 import './navbar.css';
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className="gpt3__navbar">
+			<div className="gpt3__navbar-links">
+				<div className="gpt3__navbar-links__logo">
+					<img src={logo} alt="logo" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
